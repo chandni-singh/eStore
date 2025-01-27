d862f958e54da43990c21819a55b364d917e8413
Chandni <chandni1141996@gmail.com>
2025-01-27 14:11:27 +0530

    added classes and styling to product details page

--- src/Components/ProductDetails/index.js
+++ src/Components/ProductDetails/index.js
@@ -5,27 +5,28 @@ const ProductDetails = () => {
   const location = useLocation();
   return (
     <div>
-      <div>
-        <div>
+      <div className="row container my-5 product-details-container">
+        <div className="col-5 product-img-container">
           <img
             src={require("../../assets/images/shop/" + location.state.img)}
             alt="Product"
           />
         </div>
-        <div>
-          <span>{location.state.pName}</span>
-          <div>
+        <div className="col-7 product-info">
+          <span id="product-name">{location.state.pName}</span>
+          <div className="rating-container">
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
             <i className="fa fa-star" />
           </div>
-          <div>
-            MRP: <span>{location.state.price}</span>
-            <div>Inclusive of all taxes!</div>
+          <hr />
+          <div className="product-price">
+            MRP: <span className="price">${location.state.price}</span>
+            <div style={{ fontSize: "0.8em" }}>Inclusive of all taxes!</div>
           </div>
-          <div>
+          <div className="my-3 product-description">
             <span>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
               reiciendis rem iure molestias maiores, ipsum tenetur. Nobis vero,
@@ -33,12 +34,13 @@ const ProductDetails = () => {
               voluptates tempore deserunt?
             </span>
           </div>
-          <div>
-            <div>
-              <div>
+
+          <div className="my-5">
+            <div className="btn btn-warning cart-button">
+              <div className="cart-icon-container">
                 <i className="fa fa-shopping-cart" />
               </div>
-              <div>
+              <div className="cart-text-container">
                 <p>Add to Cart</p>
               </div>
             </div>
